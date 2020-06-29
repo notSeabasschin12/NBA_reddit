e398d2af25ae9de9bacfd36776d8dd7ad56a6232
notSeabasschin12 <53022785+notSeabasschin12@users.noreply.github.com>
2020-06-29 15:02:16 -0500

    Updates three new files and added one new file
    nameMatching.py, and main.py are updated files. Added new function to nameMatching to output a csv file with separate comments and a matrix that shows whether or not a player is mentioned in said comment. main.py was updated to run new functions.
    handCodeCompare.py is a new file which compares machine code (that I created) to a ground truth file in order to calculate recall and precision.

--- main.py
+++ main.py
@@ -4,50 +4,70 @@ comment data and places them according to the global ID. It then takes all of th
 named entities mentioned in the scrapped file and narrows down the list to those
 mentioning players from the New York Knicks basketball team.
 
-I used the allennlp module's named entity recognition model to extract mentions
+I used regular expressions and the re Python module to extract mentions
 of named entities from the data that I collected and stored in the program. I
 used the pandas module which provided methods to input and manipulate data from
 csv files and then export new data into csv files.
 
 Creator: Sebastian Guo
-Last modified: June 8 2020
 """
-import extraction
-import nameMatching
-import allennlp
 import pandas
-from allennlp.predictors.predictor import Predictor
-import allennlp_models.ner.crf_tagger
+import nameMatching
+import extraction
+import extraction_v2
+import handCodeCompare
 
 def main():
     """
     Main function to run for research. Its purpose is detialed in the file comment.
     """
+
     with open("/home/sebastianguo/Documents/Research/data/smallSample.csv", newline='') as csvfile1:
         reader = pandas.read_csv(csvfile1)
     with open("/home/sebastianguo/Documents/Research/data/knick_players.csv", newline='') as csvfile2:
         reader2 = pandas.read_csv(csvfile2)
 
-
     # Creates a 2D list with list entries that are either named entities according
     # to their global/local IDs or markers to designate a comment without a
     # named entity.
-    commentDataList = extraction.extractColData(reader, reader2)
-    # globalIDList = extraction.getGlobalID(reader)
-    # # Creates csv files for each Reddit thread with all named entities mentioned.
-    # # The names of the csvfiles are the global IDs for the threads.
-    # for term in globalIDList:
-    #     extraction.createDataFrame(term, commentDataList)
+    commentDataList = extraction_v2.extractColData(reader, reader2)
+    globalIDList = extraction_v2.getGlobalID(reader)
+    playerList = nameMatching.findPlayerNames(reader2)
 
-    # # Creates a csv file named total.csv that takes the commentDataList and finds
-    # # the total times each named entity is mentioned.
-    # nameMatching.aggregate(commentDataList)
-    #
-    # # Creates a csv file named mentions.csv that builds upon the csv file
-    # # outputted by the function aggregate() but has columns that mark whether or
-    # # not the named entity corresponds to a player from the output list of
-    # # findPlayerNames.
-    # nameMatching.playerMentions(reader2, nameMatching.findPlayerNames(reader2))
+    # Creates a csv file named total.csv that takes the commentDataList and finds
+    # the total times each named entity is mentioned.
+    nameMatching.aggregate(commentDataList)
+    with open("/home/sebastianguo/Documents/Research/data/total.csv", newline='') as csvfile3:
+        reader3 = pandas.read_csv(csvfile3)
+    # Creates a csv file named mentions.csv that builds upon the csv file
+    # outputted by the function aggregate() but has columns that mark whether or
+    # not the named entity corresponds to a player from the output list of
+    # findPlayerNames.
+    nameMatching.playerMentions(reader2, reader3, playerList)
+
+    # Creates a csv file with all of the separate comments and with columns marking
+    # whether or not a certain comment contains a mention of a player
+    nameMatching.commentPlayers(reader, reader2, playerList, commentDataList)
+    with open("/home/sebastianguo/Documents/Research/data/commentMentions.csv", newline='') as csvfile4:
+        reader4 = pandas.read_csv(csvfile4)
+    with open("/home/sebastianguo/Documents/Research/data/handCodeSample.csv", newline='') as csvfile5:
+        reader5 = pandas.read_csv(csvfile5)
+
+    # Creates csv files for each Reddit thread/game with the named entities mentioned.
+    # Creates csv files for each Reddit thread/game with comments tagged if they
+    # contain a player mention.
+    # The names of the csvfiles are the global IDs for the threads.
+    for term in globalIDList:
+        extraction_v2.createDataFrame(term, commentDataList)
+        nameMatching.commentPlayersGlob(reader4, playerList, term)
+
+    # Modifies reader5 (which contains handCodeData comment by comment) to include
+    # for each player column the true positives, false positives, true negatives,
+    # precision, and recall found by comparing the ground truth, or hand code data
+    # and the code ran by the machine (the code I wrote). These data points are
+    # meant to test how well the machine code found and marked named entities
+    # within separate comments.
+    handCodeCompare.compareFiles(reader4, reader5, playerList)
 
 if __name__ == '__main__':
     main()
--- nameMatching.py
+++ nameMatching.py
@@ -3,11 +3,12 @@ Module to match named entities from csv files to players from the New York
 Knicks roster.
 
 Creator: Sebastian Guo
-Last modified: June 5 2020
 """
-import extraction
+import extraction_v2
 import commentData
 import pandas
+import numpy
+import csv
 
 def aggregate(commentDataList):
     """
@@ -66,55 +67,51 @@ def findPlayerNames(playerNameFile):
         lst.append(players[index])
     return lst
 
-def playerMentions(playerNameFile, playerList):
+def playerMentions(playerNameFile, totalFile, playerList):
     """
     Creates a csv file that contains whether or not a named entity from the inputted
     csvfile refers to a player on the New York Knicks Basketball team. The
     inputted csvfile is one that is created from the method aggregate in the
-    module nameMatching and contains the total mentions of a named entity. While
-    the file created has the same three columns as the inputted csvfile, this
-    function still creates a different csv file. Csvfile2 contains data about
-    the names of the New York Knicks basketball team and any associated nicknames.
-
-    If the csvfile does not contain the three necessary columns, return
-    a FormatError.
+    module nameMatching. While the file created has the same three columns as the
+    inputted csvfile, this function still creates a different csv file. playerNameFile
+    contains data about the names of the New York Knicks basketball team and any
+    associated nicknames. If the csvfile does not contain the three necessary columns,
+    return a FormatError.
 
     Parameter playerNameFile: a reader object that contains information about player
     names and nicknames.
     Precondition: must be a DataFrame object created by the pandas module.
 
+    Parameter totalFile: a reader object that contains information about the total
+    times a player is mentioned.
+    Precondition: must be a DataFrame object created by the pandas module.
+
     Parameter playerList: a list of strings with players to compare the csvfile to.
     Precondition: playerList must be a list with string entries.
     """
-    assert type(playerNameFile) == pandas.core.frame.DataFrame, \
-        repr(playerNameFile) + " is not a csv reader object."
-    assert type(playerList) == list, repr(playerList) + " is not a list."
-    for term in playerList:
-        assert type(term) == str, repr(term) + " is not a string."
-    with open("/home/sebastianguo/Documents/Research/data/total.csv", newline='') as csvfile:
-        totalFile = pandas.read_csv(csvfile)
+    _assertionPlayerMentions(playerNameFile, totalFile, playerList)
+    endDict = {}
     try:
         names = totalFile["named entity"]
-        cat = totalFile["category"]
-        ment = totalFile["mentions"]
+        endDict["named entity"] = names
+        endDict["category"] = totalFile["category"]
+        endDict["mentions"] = totalFile["mentions"]
     except:
         raise commentData.FormatError("The csv file does not contain the correct headers.")
-
-    endDict = {}
-    endDict["named entity"] = names
-    endDict["category"] = cat
-    endDict["mentions"] = ment
     length = len(names)
     rowsStuff = []
     for player in playerList:
         endDict[player] = [""] * length
+
+    nameList = _createList(playerNameFile, "Player")
+    listFSho = _createList(playerNameFile, "first short")
+    listLSho = _createList(playerNameFile, "last short")
+    listNick = _createList(playerNameFile, "nicknames")
     for index in range(totalFile.shape[0]):
-        name = _matchPlayer(playerNameFile, names[index])
+        name = _matchPlayer(playerNameFile, names[index], nameList, listFSho, listLSho, listNick)
         if name != "":
             endDict[name][index] = 1
             rowsStuff.append(index)
-
-
     df = pandas.DataFrame(endDict)
     for rowNum in rowsStuff:
         target_row = rowNum
@@ -122,8 +119,113 @@ def playerMentions(playerNameFile, playerList):
         df = df.iloc[idx]
     df.to_csv(r'/home/sebastianguo/Documents/Research/data/mentions.csv', index=False)
 
+def commentPlayers(rawDataFile, playerNameFile, playerList, commentDataList):
+    """
+    Create a csv file that contains individual comments and their unique global
+    and local identifiers, plus columns that mark whether or not the comment
+    has a mention of a player on the New York Knicks.
 
-def _matchPlayer(playerNameFile, namedEntity): #should be a private function
+    The file with the raw scrapped data must be formatted as following:
+    If one of the column names does not exist, an exception is returned. If the
+    columns have headers but they are not in the first row, return an exception.
+    If two columns exist with the same header, the function only looks at the
+    column with the smallest index and should still return true provided all three
+    headers exist. The global_ID and localI_ID column must have integer types.
+
+    Parameter rawDataFile: the file that contains the raw scrapped data.
+    Precondition: must be a reader object from the pandas module. It must contain
+    the three column headers global_ID, local_ID and comment.
+
+    Parameter playerNameFile: a reader object that contains information about player
+    names and nicknames.
+    Precondition: must be a DataFrame object created by the pandas module.
+
+    Parameter playerList: a list of strings with players to compare the csvfile to.
+    Precondition: playerList must be a list with string entries.
+
+    Parameter commentDataList: A list containing named entities and their types.
+    Precondition: Must be a two-dimensional list with inner entities as lists.
+    The inner entries can either be a list with four terms (global ID, local ID,
+    named entity, category) or a list with two terms (global ID and local ID).
+    """
+    _assertionCommentPlayers(rawDataFile, playerNameFile, playerList, commentDataList)
+    aggDict = {}
+    aggDict["global_ID"] = rawDataFile["global_ID"]
+    aggDict["local_ID"] = rawDataFile["local_ID"]
+    aggDict["comment"] = rawDataFile["comment"]
+    length = len(rawDataFile["global_ID"])
+    for player in playerList:
+        aggDict[player] = [""] * length
+
+    nameList = _createList(playerNameFile, "Player")
+    listFSho = _createList(playerNameFile, "first short")
+    listLSho = _createList(playerNameFile, "last short")
+    listNick = _createList(playerNameFile, "nicknames")
+    for index in range(len(aggDict["global_ID"])):
+        aggDict = _addPlayerMentions(aggDict, playerNameFile, index, aggDict["global_ID"][index],
+            aggDict["local_ID"][index], commentDataList, nameList, listFSho, listLSho, listNick)
+    df = pandas.DataFrame(aggDict)
+    df.to_csv(r'/home/sebastianguo/Documents/Research/data/commentMentions.csv', index=False)
+
+def commentPlayersGlob(commentFile, playerList, global_ID):
+    """
+    A function to create separate csv files sorted by global ID or different games.
+    Each csv file contains information similar to the csv file created by
+    commentPlayers with comments and a matrix showing whether the comment contains
+    a mention of a player name. The format of commentFile should contain the headers
+    "global_ID", "local_ID", "comment", and the players from playerList. The
+    function nameMatching.commentPlayers should ensure that these headers exist.
+
+    Paramter commentFile: a file created by commentPlayers.
+    Precondition: commentFile is a csv reader object.
+
+    Parameter playerList: a list of strings with players to compare the csvfile to.
+    Precondition: playerList must be a list with string entries.
+
+    Parameter global_ID: the global ID which all of the comments outputted by this
+    function must have.
+    Precondition: global_ID is an integer greater than zero.
+    """
+    assert type(commentFile) == pandas.core.frame.DataFrame, \
+        repr(commentFile) + " is not a reader object."
+    assert type(playerList) == list, repr(playerList) + " is not a list."
+    for term in playerList:
+        assert type(term) == str, repr(term) + " is not a string."
+    assert type(global_ID) == int and global_ID > 0, repr(global_ID) + " is not an int greater than zero."
+
+    dfDict = {"global_ID":[], "local_ID":[], "comment":[]}
+    glob = commentFile["global_ID"]
+    loc = commentFile["local_ID"]
+    comm = commentFile["comment"]
+    playerMatrix = _createMatrix(numpy.empty((0, len(commentFile)), int), commentFile, playerList)
+
+    indexList = []
+    for index in range(commentFile.shape[0]):
+        if glob[index] == global_ID:
+            dfDict["global_ID"].append(glob[index])
+            dfDict["local_ID"].append(loc[index])
+            dfDict["comment"].append(comm[index])
+            indexList.append(index)
+    for columnInd in range(len(playerMatrix)):
+        dfDict[playerList[columnInd]] = []
+        for rowInd in indexList:
+            dfDict[playerList[columnInd]].append(playerMatrix[columnInd][rowInd])
+    df = pandas.DataFrame(dfDict)
+    df.to_csv(r'/home/sebastianguo/Documents/Research/data/commentMentionsByGame/' + str(global_ID) + ".csv", index=False)
+
+def _createMatrix(matrix, commentFile, playerList):
+    """
+    Create a two-dimensional matrix to represent whether or not a table of
+    comments contain mentions of certain players in them. A "1" means that the
+    comment does have a mention and a "0" means the comment doesn't. The matrix
+    is in column major order.
+    """
+    for player in playerList:
+        matrix = numpy.append(matrix, [commentFile[player]], axis=0)
+    matrix = numpy.nan_to_num(matrix)
+    return matrix
+
+def _matchPlayer(playerNameFile, namedEntity, nameList, listFSho, listLSho, listNick): #should be a private function
     """
     Returns, if any, the player that the nameEntity refers to in a csvfile.
     containing columns with full names, first and last names, and their
@@ -132,54 +234,35 @@ def _matchPlayer(playerNameFile, namedEntity): #should be a private function
     """
     try:
         fullNames = playerNameFile["Player"]
-        nicknames = playerNameFile["nicknames"]
         first = playerNameFile["first"]
         last = playerNameFile["last"]
-        firstShort = playerNameFile["first short"]
-        lastShort = playerNameFile["last short"]
+        playerNameFile["nicknames"]
+        playerNameFile["first short"]
+        playerNameFile["last short"]
     except:
         raise commentData.FormatError("The csv file does not contain the correct headers.")
 
-    nicknameList = createList(playerNameFile, "nicknames")
-    fShortList = createList(playerNameFile, "first short")
-    lShortList = createList(playerNameFile, "last short")
-
     player = ""
     for index in range(playerNameFile.shape[0]):
-        totalLst = [nicknameList[index], fShortList[index], lShortList[index]]
-        if first[index] == namedEntity.upper() or last[index] == namedEntity.upper():
-            # check if full first or last name matches up. upper() ensures that
-            # the function matches named entities regardless of letter case.
-            player = fullNames[index]
+        totalLst = [listFSho[index], listLSho[index], listNick[index]]
+        if namedEntity == fullNames[index] or namedEntity == first[index] or namedEntity == last[index]:
+            player = nameList[index]
+            break
         for entry in totalLst:
-            if type(entry) == list:
-                if namedEntity in entry:
-                    player = fullNames[index]
-            else:
-                if entry == namedEntity:
-                    player = fullNames[index]
+            if namedEntity in entry:
+                player = nameList[index]
     return player
 
-def createList(playerNameFile, colName):
+def _createList(playerNameFile, colName):
     """
     Returns and creates a two-dimensional list according to the column name
     passed in as a parameter. Used for creating a list of nicknames, shortened
     first or last names. The reason a list is being created is for easier indexing
     into rows later on in the function matchPlayer.
-
-    Some players might have multiple nicknames or shortened names. When iterating
-    through the csv file, the reader object returns a string similar to
-    "name 1, name 2" if the player has multiple names. If a player does have
-    multiple names, then the subsequent columns indicies will be shifted down one.
-    So creating a list of the names where an inner list is created for a player
-    with multiple nicknames prevents index errors.
-
-    A similar thing can happen with names with multiple words (the multiple words
-    might be seen as separate names and be assigned separate indices).
+    Some players might have multiple nicknames or shortened names.
 
     Parameter playerNameFile: playerNameFile is the csv file contianing the column colName.
     Precondition: must be a reader object from the pandas module.
-
     Paramter colName: the name of the column in nameFile to create the list for.
     Precondition: must be a string
     """
@@ -195,9 +278,53 @@ def createList(playerNameFile, colName):
             returnList.append("")
         elif "," in items[index]:
             lstNames = items[index].split(",")
-            # Add a list into nicknameList if a player has multiple nicknames
+            # Add a list into returnList if a player has multiple names
             returnList.append(lstNames)
         else:
-            lstNames = items[index].split(",")
-            returnList += lstNames
+            returnList += items[index].split(",")
     return returnList
+
+def _addPlayerMentions(dfDictionary, playerNameFile, index, glob, loc, commentDataList, nameList, listFSho, listLSho, listNick):
+    """
+    Add to the dfdictionary any player mentions.
+    """
+    for entity in commentDataList:
+        name = ''
+        if len(entity) == 4 and entity[0] == glob and entity[1] == loc:
+            name = _matchPlayer(playerNameFile, entity[2], nameList, listFSho, listLSho, listNick)
+        if name != '':
+            for key in dfDictionary:
+                dfDictionary[name][index] = "1"
+    return dfDictionary
+
+def _assertionPlayerMentions(playerNameFile, totalFile, playerList):
+    """
+    Function to test assertions for function playerMentions.
+    """
+    assert type(playerNameFile) == pandas.core.frame.DataFrame, \
+        repr(playerNameFile) + " is not a csv reader object."
+    assert type(totalFile) == pandas.core.frame.DataFrame, \
+        repr(totalFile) + " is not a csv reader object."
+    assert type(playerList) == list, repr(playerList) + " is not a list."
+    for term in playerList:
+        assert type(term) == str, repr(term) + " is not a string."
+
+def _assertionCommentPlayers(rawDataFile, playerNameFile, playerList, commentDataList):
+    """
+    Function to test assertions for function commentPlayers.
+    """
+    assert type(rawDataFile) == pandas.core.frame.DataFrame, \
+        repr(rawDataFile) + " is not a csv reader object."
+    if not extraction_v2.checkColIndices(rawDataFile):
+        raise commentData.FormatError("The csv file is either incorrectly formatted or a column header is missing.")
+    if (rawDataFile["global_ID"].dtypes != int or rawDataFile["local_ID"].dtypes != int):
+        raise TypeError("The types of the columns are incorrect.")
+    assert type(playerNameFile) == pandas.core.frame.DataFrame, \
+        repr(playerNameFile) + " is not a csv reader object."
+    assert type(playerList) == list, repr(playerList) + " is not a list."
+    for term in playerList:
+        assert type(term) == str, repr(term) + " is not a string."
+    assert type(commentDataList) == list, repr(commentDataList) + " is not a list."
+    for term in commentDataList:
+        assert type(term) == list, repr(term) + " is not a list."
+        assert len(term) == 2 or len(term) == 4, repr(term) + " is not of correct length."
